27ab330b20519bfe813b0d49fa4c61f437864a26
HiteshPatel10 <hpatel.hp2001@gmail.com>
2023-02-28 17:30:13 +0530

    not finished

--- utility/amazon/amazon_senti.py
+++ utility/amazon/amazon_senti.py
@@ -33,11 +33,11 @@ def predict_sentiment(model, vectorizer, text):
 
 def senti():
 
-    df = pd.read_csv('../../data/am1-train.csv', parse_dates=['date'])
+    df = pd.read_csv('./data/am1-train.csv', parse_dates=['date'])
     df.dropna(subset=['content'], inplace=True)
-    filename = "../../model/amazon/amazon-logistic.joblib"
+    filename = "./model/amazon/amazon-logistic.joblib"
     model = joblib.load(filename)
-    vectorizer = joblib.load("../../model/amazon/amazon-vector-logistic.joblib")
+    vectorizer = joblib.load("./model/amazon/amazon-vector-logistic.joblib")
 
 
     df['sentiment'] = df['content'].apply(lambda x: predict_sentiment(model, vectorizer, x))
@@ -47,69 +47,64 @@ def senti():
     negative = df[df['sentiment'] == -1]
     neutral = df[df['sentiment'] == 0]
 
-    # Review no histogram
-    fig = px.histogram(df, x="rating")
-    fig.update_traces(marker_color="turquoise",marker_line_color='rgb(8,48,107)',marker_line_width=1.5)
-    fig.update_layout(title_text='Product Score')
-    pio.write_image(fig, 'product_score.png')
+    # Histogram 
+    sentiment_counts = df['sentiment'].value_counts()
+    sentiment_df = pd.DataFrame({'Sentiment': sentiment_counts.index, 'Count': sentiment_counts.values})
+    fig = px.histogram(sentiment_df, x='Sentiment', y='Count', color='Sentiment', color_discrete_sequence=['green', 'gray', 'red'])
+    fig.update_layout(title_text='Sentiment Analysis', xaxis_title='Sentiment', yaxis_title='Count')
+    fig.write_html('./static/amazon/sentiment_histogram.html')
+
 
     # Create stopword list:
     stopwords = set(STOPWORDS)
     stopwords.update(["br", "href", "good", "great"])
 
-    ## good and great removed because they were included in negative sentiment
-    pos = " ".join(str(review) for review in positive.content)
-    wordcloud2 = WordCloud(stopwords=stopwords).generate(pos)
-    plt.imshow(wordcloud2, interpolation='bilinear')
-    plt.axis("off")
+    if len(positive) > 0:
+        pos = " ".join(str(review) for review in positive.content)
+        wordcloud2 = WordCloud(stopwords=stopwords).generate(pos)
+        plt.imshow(wordcloud2, interpolation='bilinear')
+        plt.axis("off")
+        plt.savefig('./static/amazon/wordcloud_positive.png', transparent=True)
 
-    #Negative review wordcloud
-    neg = " ".join(str(review) for review in negative.content)
-    wordcloud3 = WordCloud(stopwords=stopwords).generate(neg)
-    plt.imshow(wordcloud3, interpolation='bilinear')
-    plt.axis("off")
-    plt.savefig('wordcloud33.png')
+    if len(negative) > 0:
+        neg = " ".join(str(review) for review in negative.content)
+        wordcloud3 = WordCloud(stopwords=stopwords).generate(neg)
+        plt.imshow(wordcloud3, interpolation='bilinear')
+        plt.axis("off")
+        plt.savefig('./static/amazon/wordcloud_negative.png', transparent=True)
 
-    #Negative review wordcloud
-    neu= " ".join(str(review) for review in neutral.content)
-    wordcloud4 = WordCloud(stopwords=stopwords).generate(neu)
-    plt.imshow(wordcloud4, interpolation='bilinear')
-    plt.axis("off")
-    plt.savefig('wordcloud44.png')
+    if len(neutral) > 0:
+        neu = " ".join(str(review) for review in neutral.content)
+        wordcloud4 = WordCloud(stopwords=stopwords).generate(neu)
+        plt.imshow(wordcloud4, interpolation='bilinear')
+        plt.axis("off")
+        plt.savefig('./static/amazon/wordcloud_neutral.png', transparent=True)
+
 
     # Group the reviews by date and count the number of reviews on each day for each sentiment
-    pos_reviews_per_day = positive.groupby(positive['date'].dt.date)['rating'].count()
-    neg_reviews_per_day = negative.groupby(negative['date'].dt.date)['rating'].count()
-    neu_reviews_per_day = neutral.groupby(neutral['date'].dt.date)['rating'].count()
+    pos_reviews_per_day = positive.groupby(positive['date'].dt.date)['content'].count()
+    neg_reviews_per_day = negative.groupby(negative['date'].dt.date)['content'].count()
+    neu_reviews_per_day = neutral.groupby(neutral['date'].dt.date)['content'].count()
 
     # Group the reviews by date and count the number of reviews on each day
-    reviews_per_day = df.groupby(df['date'].dt.date)['rating'].count()
+    reviews_per_day = df.groupby(df['date'].dt.date)['content'].count()
 
 
 
     # Create the time series plot for each sentiment
-    fig = plt.figure(figsize=(12,6))
-    plt.plot(pos_reviews_per_day, label='Positive')
-    plt.plot(neg_reviews_per_day, label='Negative')
-    plt.plot(neu_reviews_per_day, label='Neutral')
-    plt.xlabel('Date', fontsize=14)
-    plt.ylabel('Number of Reviews', fontsize=14)
-    plt.title('Amazon Reviews Time Series Plot', fontsize=16)
-    plt.xticks(fontsize=12, rotation=45)
-    plt.legend(fontsize=12)
-    plt.savefig('time_series.png')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=pos_reviews_per_day.index, y=pos_reviews_per_day.values, name='Positive'))
+    fig.add_trace(go.Scatter(x=neg_reviews_per_day.index, y=neg_reviews_per_day.values, name='Negative'))
+    fig.add_trace(go.Scatter(x=neu_reviews_per_day.index, y=neu_reviews_per_day.values, name='Neutral'))
+    fig.update_layout(title_text='Amazon Reviews Time Series Plot (Sentiment)', xaxis_title='Date', yaxis_title='Number of contents')
+    # fig.write_image('./static/amazon/time_series_sentiment.png')
+    fig.write_html('./static/amazon/time_series_sentiment.html')
 
     # Create the time series plot for reviews
-    fig = plt.figure(figsize=(12,6))
-    plt.plot(reviews_per_day)
-    plt.xlabel('Date', fontsize=14)
-    plt.ylabel('Number of Reviews', fontsize=14)
-    plt.title('Amazon Reviews Time Series Plot', fontsize=16)
-    plt.xticks(fontsize=12, rotation=45)
-    plt.savefig('time_series.png')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=reviews_per_day.index, y=reviews_per_day.values, name='Reviews'))
+    fig.update_layout(title_text='Amazon Reviews Time Series Plot', xaxis_title='Date', yaxis_title='Number of Reviews')
+    # fig.write_image('./static/amazon/time_series.png')
+    fig.write_html('./static/amazon/time_series.html')
 
-
-
-if __name__ == "__main__":
-    senti() 
 

14ef1250fee353531b21ec8ce54bcbb508311926
HiteshPatel10 <hpatel.hp2001@gmail.com>
2023-03-01 13:08:02 +0530

    b

--- utility/amazon/amazon_review_scrapper.py
+++ utility/amazon/amazon_review_scrapper.py
@@ -16,6 +16,7 @@ def scrape(url):
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
     ]
+    
     user_agent = random.choice(user_agent_list)
     headers = {
         'authority': 'www.amazon.com',
